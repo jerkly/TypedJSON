2b8dbcbd0e051842fb75464449a69853c431b470
Nikolay Martynenko <martynenko.nikolay@gmail.com>
2016-09-11 15:12:02 +0300

    Rebuilded

--- js/tests/single-class.js
+++ js/tests/single-class.js
@@ -1,3 +1,4 @@
+"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -7,41 +8,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-(function (factory) {
-    if (typeof module === 'object' && typeof module.exports === 'object') {
-        var v = factory(require, exports); if (v !== undefined) module.exports = v;
+var typed_json_1 = require("../typed-json");
+var Person = (function () {
+    function Person() {
     }
-    else if (typeof define === 'function' && define.amd) {
-        define(["require", "exports", "../typed-json"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    var typed_json_1 = require("../typed-json");
-    var Person = (function () {
-        function Person() {
-        }
-        Person.prototype.getFullname = function () {
-            return this.firstName + " " + this.lastName;
-        };
-        __decorate([
-            typed_json_1.JsonMember, 
-            __metadata('design:type', String)
-        ], Person.prototype, "firstName", void 0);
-        __decorate([
-            typed_json_1.JsonMember, 
-            __metadata('design:type', String)
-        ], Person.prototype, "lastName", void 0);
-        Person = __decorate([
-            typed_json_1.JsonObject, 
-            __metadata('design:paramtypes', [])
-        ], Person);
-        return Person;
-    }());
-    function test(log) {
-        var person = typed_json_1.TypedJSON.parse('{ "firstName": "John", "lastName": "Doe" }', Person);
-        person instanceof Person; // true
-        person.getFullname(); // "John Doe"
-        return person.getFullname() === "John Doe" && person instanceof Person;
-    }
-    exports.test = test;
-});
+    Person.prototype.getFullname = function () {
+        return this.firstName + " " + this.lastName;
+    };
+    __decorate([
+        typed_json_1.JsonMember, 
+        __metadata('design:type', String)
+    ], Person.prototype, "firstName", void 0);
+    __decorate([
+        typed_json_1.JsonMember, 
+        __metadata('design:type', String)
+    ], Person.prototype, "lastName", void 0);
+    Person = __decorate([
+        typed_json_1.JsonObject, 
+        __metadata('design:paramtypes', [])
+    ], Person);
+    return Person;
+}());
+function test(log) {
+    var person = typed_json_1.TypedJSON.parse('{ "firstName": "John", "lastName": "Doe" }', Person);
+    person instanceof Person; // true
+    person.getFullname(); // "John Doe"
+    return person.getFullname() === "John Doe" && person instanceof Person;
+}
+exports.test = test;
+//# sourceMappingURL=single-class.js.map
